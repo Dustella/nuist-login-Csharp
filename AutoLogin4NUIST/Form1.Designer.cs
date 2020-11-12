1c1abc06e9c162e7663f52c98ac6308e568e5d6e
星尘 <30797133+CaptSkywalker@users.noreply.github.com>
2020-11-12 19:52:11 +0800

    Add files via upload
    A demo Version.
    不成熟的一次尝试，献给C#和一个没什么用的想法。
    已经可以实现输入密码登录，和网页类似
    下一步是记录账户密码和设置开机自启、自动登录

---
+++ AutoLogin4NUIST/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace AutoLogin4NUIST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.UsernameInput = new System.Windows.Forms.TextBox();
+            this.PasswordInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(256, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UsernameInput
+            // 
+            this.UsernameInput.AcceptsTab = true;
+            this.UsernameInput.AllowDrop = true;
+            this.UsernameInput.Location = new System.Drawing.Point(149, 104);
+            this.UsernameInput.Name = "UsernameInput";
+            this.UsernameInput.Size = new System.Drawing.Size(233, 21);
+            this.UsernameInput.TabIndex = 1;
+            this.UsernameInput.WordWrap = false;
+            this.UsernameInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.AcceptsTab = true;
+            this.PasswordInput.Location = new System.Drawing.Point(145, 159);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.PasswordChar = '*';
+            this.PasswordInput.Size = new System.Drawing.Size(236, 21);
+            this.PasswordInput.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "CLose";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "DomainType";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 206);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(240, 20);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 309);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.UsernameInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AutoLoginTool-Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox UsernameInput;
+        private System.Windows.Forms.TextBox PasswordInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ AutoLogin4NUIST/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoLogin4NUIST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()           
+           
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("中国移动");
+            comboBox1.Items.Add("中国电信");
+            comboBox1.Items.Add("中国联通");
+        }
+        public void ComboBoxForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = UsernameInput.Text;
+            string password = PasswordInput.Text;
+            string domain = null;
+            string temp = comboBox1.Text;
+            switch(temp)
+            {
+                case "中国移动":
+                    domain = "CMCC";
+
+                    break;
+                case "中国电信":
+                    domain = "ChinaNet";
+                    break;
+                case "中国联通":
+                    domain = "Unicom";
+                    break;
+
+            }
+            POST p = new POST();
+            p.Post(username, password,domain);
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
